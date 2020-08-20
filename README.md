300b81c2175b50bd447bbbf756e1c253432da1e1
Bebatterman <2604092855@qq.c$ git statusom>
2020-08-20 15:25:38 +0800

    工程环境配置

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 # author
 李志强 叶子 李月艳 刘泽山
+
+# myapp
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
---
+++ client/src/utils/request.js
@@ -0,0 +1,15 @@
+import axios from './node_modules/axios';
+
+const request = axios.create({
+    baseURL:'http://localhost:2003/api'
+});
+
+
+// export const nsg = axios.create({
+//     baseURL:'http://www.nsg.com'
+// });
+
+// axios.get(url,config)
+// axios.post(url,data,config)
+
+export default request;
