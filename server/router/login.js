9c01283c2dd4862ac5fa0e6a920010e0ad9b8360
qiguoqicha <206812867@qq.com>
2020-08-28 20:56:54 +0800

    叶子第三次提交

--- server/router/login.js
+++ server/router/login.js
@@ -6,16 +6,11 @@ const mongo = require('../utils/mongo');
 // 登录
 router.get('/', async (req, res) => {
     let { username, password} = req.query;
-    console.log(req.query);
     let result = await mongo.find('manager', { username, password });
-    console.log(result);
     if (result.length > 0) {      
         res.send(formatData({ data: result }));
     } else {
         res.send(formatData({ code: 0 }))
     }
 })
-
-
-
 module.exports = router;
