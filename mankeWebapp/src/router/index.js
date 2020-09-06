075250a76c3fdf4b5ff3e82fcf89cf2649a3b80a
qiguoqicha <206812867@qq.com>
2020-09-06 21:22:57 +0800

    叶子第一次提交

--- mankeWebapp/src/router/index.js
+++ mankeWebapp/src/router/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/home'
-
+import request from '../utils/request';
 Vue.use(VueRouter)
 const routes = [
   {
-    path:'/',
-    redirect:{name:'Home'}
+    path: '/',
+    redirect: { name: 'Home' }
   },
   {
     path: '/home',
@@ -16,31 +16,94 @@ const routes = [
   {
     path: '/sort',
     name: 'Sort',
-    component:()=>import('@/views/sort')
+    component: () => import('@/views/sort')
   },
   {
     path: '/cart',
     name: 'Cart',
-    component:()=>import('@/views/cart')
+    meta: {
+      requiresAuth: true
+    },
+    component: () => import('@/views/cart')
   },
   {
     path: '/profile',
     name: 'Profile',
-    component:()=>import('@/views/profile')
+    meta: {
+      requiresAuth: true
+    },
+    component: () => import('@/views/profile')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/login'),
+  },
+  {
+    path: '/reg',
+    name: 'Reg',
+    component: () => import('@/views/login/reg')
+  },
+  {
+    path: '/reg',
+    name: 'Reg',
+    component: () => import('@/views/login/reg')
+  },
+  {
+    path: '/edit',
+    name: 'Edit',
+    component: () => import('@/views/profile/edit')
   }
-
-
-    // 路由按需引入(路由懒加载)
-    // component:function(){
-    //   return import('../views/About.vue')
-    // }
-    // component:()=>import('../views/Reg.vue')
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
 ]
 
 const router = new VueRouter({
   routes
 })
 
+router.beforeEach(function (to, from, next) {
+  // 判断目标路由是否需要登录才可访问
+  // if(to.meta.requiresAuth){
+  if (to.matched.some(item => item.meta.requiresAuth)) {
+    let userInfo = localStorage.getItem('currentUser') || {};
+    try {
+      userInfo = JSON.parse(userInfo);
+    } catch (err) {
+      userInfo = {};
+    }
+    console.log(userInfo);
+    // 判断当前用户信息是否包含token
+    if (userInfo.authorization) {
+
+      // 发起请求校验token的有效性
+      request.get('/jwtverify', {
+        params: {
+          authorization: userInfo.authorization
+        }
+      }).then(({ data }) => {
+        if (data.code === 0) {
+          next({
+            path: '/login',
+            query: {
+              // 跳转到登录页面，并传递目标页面路径
+              redirectTo: to.fullPath
+            }
+          });
+        }
+      })
+      next();
+    } else {
+      next({
+        path: '/login',
+        query: {
+          // 跳转到登录页面，并传递目标页面路径
+          redirectTo: to.fullPath
+        }
+      })
+    }
+  } else {
+    next();
+  }
+
+
+});
 export default router
--- mankeWebapp/src/views/profile/index.vue
+++ mankeWebapp/src/views/profile/index.vue
@@ -1,10 +1,105 @@
 <template>
-  <div>
-    profile
+  <div class="mine" style="padding: 0 6px">
+    <van-row style="text-align:center">
+      <van-col span="10">
+        <van-image
+          round
+          width="10rem"
+          height="10rem"
+          :src="avatar?avatar:'https://img.yzcdn.cn/vant/cat.jpeg'"
+          style="margin:20px auto"
+        />
+      </van-col>
+      <van-col span="14">
+        <div style="font-size:18px; margin: 50px 0 20px 0">{{username}}</div>
+        <span style="font-size:14px">您的等级：注册用户</span>
+      </van-col>
+    </van-row>
+    <div style="height: 150px; padding:5px 0;">
+      <van-row style="height: 50px">
+        <van-col span="2">
+          <van-icon name="orders-o" size="28" />
+        </van-col>
+        <van-col span="20" style="font-size:18px">我的订单</van-col>
+        <van-col span="2">
+          <van-icon name="arrow" size="24" />
+        </van-col>
+      </van-row>
+      <van-row style="text-align:center">
+        <van-col span="8">
+          <van-icon name="failure" size="50" />
+          <span style="display: block">待付款</span>
+        </van-col>
+        <van-col span="8">
+          <van-icon name="logistics" size="50" />
+          <span style="display: block">待收货</span>
+        </van-col>
+        <van-col span="8">
+          <van-icon name="completed" size="50" />
+          <span style="display: block">已完成</span>
+        </van-col>
+      </van-row>
+    </div>
+    <van-row style="height: 50px">
+      <van-col span="2">
+        <van-icon name="like-o" size="28" />
+      </van-col>
+      <van-col span="20" style="font-size:18px">我的收藏</van-col>
+      <van-col span="2">
+        <van-icon name="arrow" size="24" />
+      </van-col>
+    </van-row>
+    <van-row style="height: 50px" @click="goEdit">
+      <van-col span="2">
+        <van-icon name="edit" size="28" />
+      </van-col>
+      <van-col span="20" style="font-size:18px">修改信息</van-col>
+      <van-col span="2">
+        <van-icon name="arrow" size="24" />
+      </van-col>
+    </van-row>
+    <van-row style="height: 50px">
+      <van-col span="2">
+        <van-icon name="location-o" size="28" />
+      </van-col>
+      <van-col span="20" style="font-size:18px">收货地址</van-col>
+      <van-col span="2">
+        <van-icon name="arrow" size="24" />
+      </van-col>
+    </van-row>
+    <van-button round block type="info" @click="logout">退出</van-button>
   </div>
 </template>
 <script>
+import Vue from "vue";
+import { Image as VanImage } from "vant";
+import { Col, Row } from "vant";
+
+Vue.use(Col);
+Vue.use(Row);
+Vue.use(VanImage);
+
 export default {
-  name:'Profile'
-}
+  data() {
+    return {
+      username: "",
+    };
+  },
+  methods: {
+    goEdit() {
+      this.$router.push({ name: "Edit" });
+    },
+    logout(){
+      this.$router.push({ name: "Login" });
+      localStorage.removeItem("currentUser");
+    }
+  },
+  created() {
+    const res = JSON.parse(localStorage.getItem("currentUser"));
+    this.username = res.username;
+    this.avatar = res.avatarUrl;
+  },
+};
 </script>
+<style lang="scss">
+</style>
